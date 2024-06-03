ee204ee4b3947ad96c93a0078829e45a4eafb3ab
Christopher Newton <christopher.k.newton@me.com>
2024-06-03 11:11:48 -0700

    Mirror `ls` basic dir access errors

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,34 @@
 const std = @import("std");
 const fs = std.fs;
 
-pub fn main() !void {
+pub fn main() !u8 {
     const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
 
-    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    const args = std.process.argsAlloc(std.heap.page_allocator) catch |err| {
+        stderr.print("Memory error: {}\n", .{err}) catch {};
+        return 1;
+    };
     defer std.process.argsFree(std.heap.page_allocator, args);
 
-    const targetDir = if (args.len > 1) args[1] else "./";
+    const targetDirArg = if (args.len > 1) args[1] else "./";
 
-    var dir = try fs.cwd().openDir(targetDir, .{ .iterate = true });
+    var dir = std.fs.cwd().openDir(targetDirArg, .{ .iterate = true }) catch |err| {
+        switch (err) {
+            error.AccessDenied => stderr.print("l: {s}: Operation not permitted\n", .{targetDirArg}) catch {},
+            error.FileNotFound => stderr.print("l: {s}: No such file or directory\n", .{targetDirArg}) catch {},
+            error.NotDir => stderr.print("l: {s}: Not a directory\n", .{targetDirArg}) catch {},
+            else => {
+                stderr.print("l: {}: {s}\n", .{ err, targetDirArg }) catch {};
+            },
+        }
+        return 1;
+    };
+    defer dir.close();
+
     var iterator = dir.iterate();
-
     while (try iterator.next()) |file| {
         try stdout.print("{s}\n", .{file.name});
     }
-
-    dir.close();
+    return 0;
 }
